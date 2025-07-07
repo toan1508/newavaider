56a7699a83c0e3d728b1587e98cfcd05b9f201cb
toan1508 <toanhp1508@gmail.com>
2025-07-07 09:25:25 +0700

    change favicon to custom avaider icon

--- companyInfo.js
+++ companyInfo.js
@@ -10,5 +10,8 @@ If you have any questions or need assistance, feel free to ask. I'm here to help
     -Microsoft Teams Name: toanhp1508@gmail.com ( Dino BEAN )
 Details:
     - My name is Ho Phuoc Toan. I was born on August 15, 2012, in Vietnam. I am currently a student study coding. I studing at Nguyen Thien Thuat secondary school.
+    - Toan's mother name: Nguyen Thi Dieu Linh
+    - Toan's father name : Ho Phuoc Tu
+    - Toan's brother name : Ho Phuoc Tien
 `;
 export default companyInfo;
